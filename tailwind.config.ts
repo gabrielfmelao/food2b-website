d8a81e786705789bd62fee221573cedea4b28b4a
Marcos Santos <marcos.santos@easyb2b.io>
2024-01-25 20:52:23 -0300

    my first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+module.exports = {
+  // configure the paths to all of your source files
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    'node_modules/preline/dist/*.js',
+    './src/**/*.{html,js}',
   ],
+  
+
+  // enable dark mode via class strategy
+  darkMode: 'class',
+
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      // extend base Tailwind CSS utility classes
     },
+    
   },
-  plugins: [],
-};
-export default config;
+
+  // add plugins to your Tailwind CSS project
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('preline/plugin')
+  ]
+}
