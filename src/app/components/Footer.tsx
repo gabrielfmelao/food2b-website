a45b27755dc9daf6dd91d3023c1ced404d0521e4
Marcos Santos <marcos.santos@easyb2b.io>
2024-04-09 19:50:12 -0300

    fix a modal component

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <footer className="w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
             {/* Grid */}
             <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 mb-10">
-                <div className="col-span-full hidden lg:col-span-1 lg:block">
+                <div className="col-span-full lg:col-span-1">
                     <LogoComponent />
                     <p className="mt-3 text-xs sm:text-sm text-gray-600 dark:text-gray-400">© 2024 Food2B</p>
                 </div>
--- src/ui/Modal.tsx
+++ src/ui/Modal.tsx
@@ -10,9 +10,9 @@ export default function Modal() {
                 <svg className="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="m9 18 6-6-6-6" /></svg>
             </button>
 
-            <div id="hs-vertically-centered-modal" className="hs-overlay-backdrop-open:bg-black-950/90 backdrop-blur-sm hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden pointer-events-none overflow-y-hidden">
-                <div className="hs-overlay-open:mt-0 lg:hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-xl lg:max-w-xl xl:max-w-xl sm:w-full m-3 mx-auto min-h-full lg:min-h-[calc(100%-3.5rem)] flex items-center">
-                    <div className="w-full flex flex-col bg-white border shadow-sm rounded-none sm:rounded-xl pointer-events-auto h-[100vh] sm:h-auto">
+            <div id="hs-vertically-centered-modal" className="hs-overlay-backdrop-open:bg-black-950/90 backdrop-blur-sm hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden pointer-events-none overflow-y-auto">
+                <div className="hs-overlay-open:mt-0 sm:hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-xl md:max-w-xl xl:max-w-xl sm:w-full m-3 mx-auto max-h-[100vh] sm:min-h-[calc(100%-3.5rem)] flex items-center h-full sm:h-auto">
+                    <div className="w-full flex flex-col bg-white border shadow-sm rounded-none sm:rounded-xl pointer-events-auto h-full md:h-auto">
                         <div className="flex justify-between items-center py-6 px-4 border-b pl-8">
                             <Image
                                 src={Logo}
